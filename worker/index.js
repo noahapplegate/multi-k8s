b649dc684272af6307e91a0731046f821742f514
Noah Applegate <noahapplegate1@gmail.com>
2021-11-12 19:08:55 -0800

    initial commit

---
+++ worker/index.js
@@ -0,0 +1,21 @@
+const keys = require('./keys'); // File holding redis hostname and port
+const redis = require('redis'); // File for creating a redis client
+
+// Create a redis client
+const redisClient = redis.createClient({
+    host: keys.redisHost,
+    port: keys.redisClient,
+    retry_strategy: () => 1000
+});
+const sub = redisClient.duplicate();    // Make a duplicate of the redis client
+
+function fib(index) {
+    // On purpose slow recursive solution
+    if (index < 2) return 1;
+    return fib(index - 1) + fib(index - 2);
+}
+
+sub.on('message', (channel, message) => {
+    redisClient.hset('values', message, fib(parseInt(message)));
+});
+sub.subscribe('insert');
